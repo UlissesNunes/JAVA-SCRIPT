6399fc76f83c134637a47a61b06d4039cd09bb52
UlissesNunes <ulissesnunesx@gmail.com>
2024-12-27 12:05:05 -0300

    att da versão de teste com arrays

---
+++ javascript/Testes.gerais/pronto.js
@@ -0,0 +1,8 @@
+function tela() {
+const res= document.querySelector('div#res')
+
+let num = [ 3,9,75,56,55,353]
+for(let pos = 0 ; pos < num.length ; pos++  ) 
+res.innerHTML +=`indice é    ${pos}   e o valor dele é     ${num[pos]}`
+
+}
