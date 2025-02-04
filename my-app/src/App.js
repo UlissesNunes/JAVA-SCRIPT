9d27f944fe9cb5bd794ddc17e14a70fb5d7071d0
UlissesNunes <ulissesnunesx@gmail.com>
2025-02-04 13:02:28 -0300

    treinei react

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,33 @@
 
 import './App.css';
 
+import NewComponent from './componentes/NewComponent'
+
+
+
+
 function App() {
+
+  const name = {
+    name: 'Ulisses' 
+  }
+  const som = (a,b)=> {
+    return a + b
+
+  }
   return (
     <div>
-     <h1> Olá React / World</h1>
+    
+    
+  <h1> Olá {name.name}! React / World</h1>
+   <p><img src='./logo192.png' alt=''></img></p>
      <p>Meu primeiro código em junção do react..
      </p>
+     <p> o resultado da soma é {som(2,4)}</p>
+    <NewComponent/>
+    
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ my-app/src/componentes/NewComponent.js
@@ -0,0 +1,12 @@
+const NewComponent = () => {
+   
+    return (
+        <div>
+            <p>COPONENTE QUE FINALMENTE IMPORTEEII</p>
+        </div>
+    )
+    
+  }
+
+
+export default NewComponent
