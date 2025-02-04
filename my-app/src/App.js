2ef0cc63c1ef90d2f023ec1611b7c94e5ed8fbdb
UlissesNunes <ulissesnunesx@gmail.com>
2025-02-04 10:06:56 -0300

    instalei pack de react

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div>
+     <h1> Olá React / World</h1>
+     <p>Meu primeiro código em junção do react..
+     </p>
+    </div>
+  );
+}
+
+export default App;
