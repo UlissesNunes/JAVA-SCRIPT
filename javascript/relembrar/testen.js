2f800e9d3dfd7e070d234cdc311ce6b988ab5e97
UlissesNunes <ulissesnunesx@gmail.com>
2025-02-07 13:39:44 -0300

    Prática leva la perfection

--- javascript/relembrar/testen.js
+++ javascript/relembrar/testen.js
@@ -14,19 +14,26 @@ const soma = (a, b) => a / b
   }
 sobra(10,6)
 
-/*
- MAP()
-///////////////////////////////////////////////////////////////////////////////////////////////
 
-const numersS = [12,2, 280]
+
+//////////////// MAP ()  //////////////////////
+const nume = [23,3,4]
+const dobrar = nume.map((dobrar) => dobrar *2 )
+console.log(dobrar)
+
+
+const numersS = [23,3,4]
 const numersMULTI = numersS.map(cavalo => cavalo * 5 )
 
 console.log(numersMULTI)
 
+
+
+
 //////// DE FAHREIMHEIT PARA CELSIUS //////////// c = (f - 32) * 5 / 9  //////////
 
 
-const faheiheint = [0,54,6]
+const faheiheint = [23,3,4]
 const celsius = faheiheint.map(faheiheint => (faheiheint - 32) * 5 /9)
 console.log('De fahenheit para Celsius : ' + celsius) 
 
@@ -34,34 +41,36 @@ console.log('De fahenheit para Celsius : ' + celsius)
 
 //////////// DE CELSIUS PARA FAHRENHEIT //////// f = c * 9 / 5 + 32  //////////////////
 
-const Celsius = [2,23,5]
+const Celsius = [23,3,4]
 const fahrenheit = Celsius.map(Celsius => (Celsius * 9) / 5 + 32)
 console.log('De celsius para fahrenheit : ' + fahrenheit)
 
 
 ///////////// DE KELVIN PARA FAHRENHEIT //////////// f = (k - 273,15) * 9 / 5 + 32 ////////
 
-const Kelvin = [12,32,4,6,7]
+const Kelvin = [23,3,4]
 const fahrenheitNOVO = Kelvin.map(kelvin => (kelvin -273,15) * 9 / 5 + 32)
 console.log('De kelvin para fahenheit : ' + fahrenheitNOVO )
 
 /////////////// DE FAHREINHEIT PARA KELVIN ////////// k = (f - 32) * 9 / 5 + 273,15 /////////
 
-const fahreinheitNOVISSIMO = [34,54,1,2,5]
+const fahreinheitNOVISSIMO = [23,3,4]
 const kelvinNOVO = fahreinheitNOVISSIMO.map(fahreinheitNOVISSIMO => (fahreinheitNOVISSIMO - 32) * 5 / 9 + 273,15)
 console.log('De faheinheit para kelvin : ' + kelvinNOVO)
 
 ///////////// DE CELSIUS  PARA KELVIN //////////// k = c + 273,15  ///////////////
 
-const UltimoCelsius = [2,8,4,22]
+const UltimoCelsius = [23,3,4]
 const UltimoKelvin = UltimoCelsius.map(UltimoCelsius => (UltimoCelsius +  273,15))
 console.log('De celsius para kelvin : ' + UltimoKelvin)
 
 //////////////// DE KELVIN PARA CELSIUS //////// c = k - 273,15 //////////////
 
-const AcabouKelvin = [25,10,11,8]
+const AcabouKelvin = [23,3,4]
 const AcabouCelsius = AcabouKelvin.map(AcabouKelvin => (AcabouKelvin - 273,15))
-console.log('De kelvin para celsius : ' + AcabouCelsius) */
+console.log('De kelvin para celsius : ' + AcabouCelsius) 
+
+
 
 
 
@@ -69,38 +78,71 @@ console.log('De kelvin para celsius : ' + AcabouCelsius) */
 //////  FILTER()  //////////
 
 
-/* const shopping = [
+const shopping = [
   {nome: 'Tenis' , preco:499.87},
-  {nome: 'Camisa' , preco:235.90},
+  {nome: 'Camisa' , preco:135.90},
   {nome: 'Calca', preco:189.99} ,
   {nome: 'bone' , preco: 109.97}
 ]
 const Produto = shopping.filter(shopping => shopping.preco < 200)
 
 console.log(Produto)
-console.log(shopping) */
 
 
-const num = [2,4,88,27,4,5,3,2,16 ]
+
+const num = [2,4,88,27,4,5,3,2,16,16,37,27 ]
 const semRepetir = num.filter((atualArray , index , numArray) => numArray.indexOf(atualArray) === index)
 console.log(semRepetir)
 
 
-const nume = [23,3,4,1,11,2]
-const dobrar = nume.map((dobrar) => dobrar *2 )
-console.log(dobrar)
 
 
 
 
-const produtos1 = [
-  {lojaA: 'playstation 5' , preco: 6000.90} ,
-  {lojaB: 'xbox One' , preco: 4343.90} ,
-  {lojaC: 'Nitendo' , preco: 1989.89} 
+
+
+
+
+///////  REDUCE()  ///////////
+
+
+
+const jogadores = [
+{nome: "Neymar" , Títulos: 28 } ,
+{ nome: "Messi" , Títulos: 41} ,
+{ nome: "CristianoRonaldo" , Títulos: 55},
+{nome: "ThomasMuller" , Títulos: 32},
+{nome: "Neuer" , Títulos: 31}
+
 
 ]
 
-const promocao1 = produtos.filter(produtos =>  produtos.preco + 100)
-console.log(` ${promocao}`)
+
+const Títulos = jogadores.reduce( (jogadoresArray , TítulosArrayAtual) => jogadoresArray + TítulosArrayAtual.Títulos , 0)
+console.log(Títulos)
+
+
+
+
+///////  EVERY() ////////////////////////
+
+// verificar se todos os nomes já faleceram.
+//  Se EVERY(todos) tiverem um ano de falecimento menor do que o ANO ATUAL(2025) significa que sim todos os nomes já faleceram = TRUE , caso tenha algum que ainda está vivo ou vai falecer posteriormente, sairia no terminal = FALSE
+
+const falecimento = [
+  {nome: "Pele" , anoFalecimento: 2024 } ,
+  {nome: "Maradona" , anoFalecimento: 2024 } ,
+  {nome: "Beckenbauer" , anoFalecimento: 2024} ,
+  {nome: "Gerd MÜller" , anoFalecimento: 2023} ,
+  {nome: "FFGG" , anoFalecimento: 2021}
+]
+
+console.log(falecimento.every((ArrayFalecimento) => ArrayFalecimento.anoFalecimento < 2025 ) )
+
+
+
+
+////////   SOME()  ///////////////////////////
+
 
 
